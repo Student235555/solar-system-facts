a50474a9365d57e323bd3fb9e921848e43674b4c
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-11-10 14:19:41 +0100

    Add some display changes

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -3,7 +3,7 @@ import '../styles/Planet.css';
 
 const Planet = ({astrObj}) => {
 
-    const {name} = astrObj;
+    const {name, mass, diameter, gravity, lengthOfDay, orbitalPeriod, distanceFromSun, meanTemperature, moons} = astrObj;
 
     const [active, setActive] = useState(true);
 
@@ -28,7 +28,16 @@ const Planet = ({astrObj}) => {
         <>
             <li className='planetLiContainer'>
                 <p className={LiPlanetMain + ' ' + LiPlanetColor} onClick={() => { changeColor(); changeVisi();}}>{name}</p>
-                <div className={divInLi + ' ' + divInLiVis}>{name + " LOL"}</div>
+                <div className={divInLi + ' ' + divInLiVis}>
+                    <p>Mass: {mass} 10<sup>24</sup> kg</p>
+                    <p>Diameter: {diameter} km</p>
+                    <p>{gravity}</p>
+                    <p>{lengthOfDay}</p>
+                    <p>{orbitalPeriod}</p>
+                    <p>{distanceFromSun}</p>
+                    <p>{meanTemperature}</p>
+                    <p>{moons}</p>
+                </div>
             </li>
         </>
      );
