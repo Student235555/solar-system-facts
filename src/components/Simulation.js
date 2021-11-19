3cb04e116059911a796f60e2fd1aeeebbf891369
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-11-19 14:00:24 +0100

    Create Simulation.js

---
+++ src/components/Simulation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Simulation.css';
+import {Link} from 'react-router-dom';
+
+const Simulation = () => {
+    return ( 
+        <div>
+            <h1>Simulation</h1>
+            <Link to="/">Return</Link>
+        </div>
+     );
+}
+ 
+export default Simulation;
