1c4bf7a3dec4af99488e1c34bf13f9bce8175595
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-11-19 14:00:48 +0100

    Create Page.js and add Routes

---
+++ src/components/Page.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HomePage from './HomePage';
+import AgeCalculator from './AgeCalculator';
+import Simulation from './Simulation';
+import {Routes, Route} from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+
+const Page = () => {
+    return ( 
+        <>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="calculator" element={<AgeCalculator />} />
+            <Route path="simulation" element={<Simulation />} />
+            <Route element={<ErrorPage />} />
+          </Routes>
+        </>
+     );
+}
+ 
+export default Page;
