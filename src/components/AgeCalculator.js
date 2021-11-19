896732ab013f0f1a5ed04e6ee491d055976cbf9c
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-11-19 13:59:58 +0100

    Create AgeCalculator.js

---
+++ src/components/AgeCalculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/AgeCalculator.css';
+import {Link} from 'react-router-dom';
+
+const AgeCalculator = () => {
+    return ( 
+        <div>
+            <h1>Calculator</h1>
+            <Link to="/">Return</Link>
+        </div>
+     );
+}
+ 
+export default AgeCalculator;
