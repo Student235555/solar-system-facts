36bef7fba9f57f1eb6b473d445da7d4549e00cdb
Marcin Pawlus <marcinpawlus97@gmail.com>
2021-12-02 14:02:25 +0100

    Update simulation

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -6,6 +6,20 @@ const Simulation = () => {
     return ( 
         <div>
             <h1>Simulation</h1>
+            <div class="dots">
+                <span class="dot uranus"></span>
+                <span class="dot jupiter"></span>
+                <span class="dot earth"></span>
+                <span class="dot mercury"></span>
+                <span class="dot sun"></span>
+                <span class="dot venus"></span>
+                <span class="dot mars"></span>
+                <span class="dot saturn">
+                    <div class='line'>
+                    </div>
+                </span>
+                <span class="dot neptune"></span>
+            </div>
             <p className='returnLink'><Link to="/">Return to the main page</Link></p>
         </div>
      );
