13ab67e6ff6ca3a0d474e716e9e8101d6d14779b
Marcin Pawlus <marcinpawlus97@gmail.com>
2022-01-11 13:47:45 +0100

    Get ready to deploy to gh

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import '../styles/App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <header>
           {<Header/>}
